25d8d84fa38fc7cac1a52ef3b3ed27280df77e5e
Kamil <khamiga97@gmail.com>
2023-02-06 19:48:08 +0100

    new files

---
+++ cypress/page-objects/homePage.js
@@ -0,0 +1,32 @@
+import Urls from "./urls";
+
+const inputsHeader = '#inputs-header'
+const checkboxHeader = '#checkbox-header'
+const dropdownListHeader = '#dropdownlist-header'
+const hoversHeader = '#hovers-header'
+
+class HomePage{
+
+    clicInputsTab() {
+        cy.get(inputsHeader).click()
+    }
+
+    clickCheckboxTab() {
+        cy.get(checkboxHeader).click()
+    }
+
+    clickDropdownListTab() {
+        cy.get(dropdownListHeader).click()
+    }
+
+    clickHoversTab() {
+        cy.get(hoversHeader).click()
+    }
+
+    visitPage() {
+        const url = new Urls()
+        url.visitHomePage()
+    }
+}
+
+export default HomePage;
