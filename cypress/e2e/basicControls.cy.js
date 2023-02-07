384bb140e9f31707b2e9e1705308b34e9ae1275a
Kamil <khamiga97@gmail.com>
2023-02-07 18:14:40 +0100

    Hover and dropdown tests added

--- cypress/e2e/basicControls.cy.js
+++ cypress/e2e/basicControls.cy.js
@@ -1,6 +1,8 @@
 import InputPage from "../page-objects/inputPage"
 import HomePage from "../page-objects/homePage"
 import CheckboxPage from "../page-objects/checkboxPage"
+import DropdownListPage from "../page-objects/dropdownListPage"
+import HoversPage from "../page-objects/hoversPage"
 
 describe('my first scenario', () => {
 
@@ -27,5 +29,23 @@ describe('my first scenario', () => {
         const checkboxPage = new CheckboxPage();
         checkboxPage.checkFirstCheckbox()
         checkboxPage.checkSecondCheckbox()
+    })
+
+    it('test dropdown list', () => {
+
+        const homePage = new HomePage();
+        homePage.clickDropdownListTab()
+
+        const dropdownPage = new DropdownListPage()
+        dropdownPage.chooseDropdownListFirstOption()
+    })
+
+    it('test hover', () => {
+
+        const homePage = new HomePage();
+        homePage.clickHoversTab()
+
+        const hoversPage = new HoversPage();
+        hoversPage.hoverOverElement()
     })
 })
